16c9fbd26522be91b72b8bbc3352ae2a5e721dff
Dino-Martinez <>
2021-03-24 23:51:56 -0700

    [Added] Entity class and enforced styling

---
+++ src/entity.js
@@ -0,0 +1,159 @@
+import Transform from './transform.js'
+import Vector2 from './vector2.js'
+
+export default class Entity {
+  constructor (sprite = 'default', priority = 0) {
+    this.transform = new Transform()
+    this.sprite = sprite
+    this.ready = false
+    this.active = true
+    this.dirty = true
+    this.priority = priority
+  }
+
+  /**
+   * get x - Get the x component of this transform's position
+   *
+   * @return {number} The x component of position
+   */
+  get x () {
+    return this.transform.x
+  }
+
+  /**
+   * get y - Get the y component of this transform's position
+   *
+   * @return {number} The y component of position
+   */
+  get y () {
+    return this.transform.y
+  }
+
+  /**
+   * get scale - Get the value for this transform's scale
+   *
+   * @return {number} The value for this transform's scale
+   */
+  get scale () {
+    return this.transform.scale
+  }
+
+  get width () {
+    return this.transform.scale.x
+  }
+
+  get height () {
+    return this.transform.scale.y
+  }
+
+  /**
+   * get rotation - Get the value of this transform's rotation
+   *
+   * @return {number} The rotation value, in degrees
+   */
+  get rotation () {
+    return this.transform.rotation
+  }
+
+  /**
+   * set x - Sets the x component of this transform's position
+   *
+   * @param  {number} newX The new x component
+   */
+  set x (newX) {
+    try {
+      this.transform.x = newX
+    } catch (e) {
+      console.error(e)
+      throw e
+    }
+  }
+
+  /**
+   * set y - Sets the y component of this transform's position
+   *
+   * @param  {number} newY The new y component
+   */
+  set y (newY) {
+    try {
+      this.transform.y = newY
+    } catch (e) {
+      console.error(e)
+      throw e
+    }
+  }
+
+  /**
+   * set size - Sets the size of this transform to the given vector
+   *
+   * @param  {Vector2} newSize The vector {width, height}
+   */
+  set size (newSize) {
+    try {
+      this.transform.vectorScale(newSize)
+    } catch (e) {
+      console.error(e)
+      throw e
+    }
+  }
+
+  /**
+   * set rotation - Sets the value for this transform's rotation
+   *
+   * @param  {number} newRot The new rotation value, in degrees
+   */
+  set rotation (newRot) {
+    try {
+      this.transform.rotation = newRot
+    } catch (e) {
+      console.error(e)
+      throw e
+    }
+  }
+
+  /**
+   * scale - Scales this transform by the given factor
+   *
+   * @param  {number} scaleFactor The factor by which to scale
+   */
+  scale (scaleFactor) {
+    try {
+      this.transform.scalarScale(scaleFactor)
+    } catch (e) {
+      console.error(e)
+      throw e
+    }
+  }
+
+  /**
+   * preRender - Ensures that this entity is prepared to be rendered
+   *
+   */
+  preRender () {
+    this.ready = true
+  }
+
+  /**
+   * render - Renders this entity on the given context
+   *
+   * @param  {CanvasRenderingContext2D} ctx the rendering context for your HTML canvas
+   */
+  render (ctx) {
+    if (this.ready && this.dirty && this.active) {
+      // Draw using canvas context
+      ctx.fillStyle = '#FFBAD2'
+      ctx.rotate(this.rotation)
+      ctx.fillRect(this.x, this.y, this.width, this.height)
+      ctx.rotate(-this.rotation)
+    }
+  }
+
+  /**
+   * postRender - Cleans this entity and unreadies it
+   *
+   */
+  postRender () {
+    this.ready = false
+    this.dirty = false
+  }
+}
--- src/transform.js
+++ src/transform.js
@@ -1,109 +1,109 @@
-import Vector2 from "./vector2.js"
+import Vector2 from './vector2.js'
 export default class Transform {
-    constructor() {
-        this._position = new Vector2()
-        this._rotation = 0
-        this._scale = new Vector2()
-    }
+  constructor () {
+    this._position = new Vector2()
+    this._rotation = 0
+    this._scale = new Vector2(1, 1)
+  }
 
-    /**
-     * @returns {number}
-     */
-    get position() {
-        return this._position
-    }
+  /**
+   * @returns {number}
+   */
+  get position () {
+    return this._position
+  }
 
-    /**
-     * @returns {number}
-     */
-    get rotation() {
-        return this._rotation
-    }
+  /**
+   * @returns {number}
+   */
+  get rotation () {
+    return this._rotation
+  }
 
-    /**
-     * @returns {number}
-     */
-    get scale() {
-        return this._scale
-    }
+  /**
+   * @returns {number}
+   */
+  get scale () {
+    return this._scale
+  }
 
-    /**
-     * @param {Vector2} newPosition
-     */
-    set position(newPosition) {
-        if (newPosition instanceof Vector2) {
-            this._position = newPosition
-        } else {
-            throw new TypeError()
-        }
+  /**
+   * @param {Vector2} newPosition
+   */
+  set position (newPosition) {
+    if (newPosition instanceof Vector2) {
+      this._position = newPosition
+    } else {
+      throw new TypeError()
     }
-    
-    /**
-     * @param {number} newRotation
-     */
-    set rotation(newRotation) {
-        if (typeof newRotation == 'number') {
-            this._rotation = newRotation % 360
-        } else {
-            throw new TypeError()
-        }
-    }
+  }
 
-    /**
-     * @param {Vector2} newScale
-     */
-    set scale(newScale) {
-        if (newScale instanceof Vector2) {
-            this._scale = newScale
-        } else {
-            throw new TypeError()
-        }
+  /**
+   * @param {number} newRotation
+   */
+  set rotation (newRotation) {
+    if (typeof newRotation === 'number') {
+      this._rotation = newRotation % 360
+    } else {
+      throw new TypeError()
     }
+  }
 
-    /**
-     * @param {Vector2} delta 
-     */
-    translate(delta) {
-        if (delta instanceof Vector2) {
-            this.position.x += delta.x
-            this.position.y += delta.y
-        } else {
-            throw new TypeError()
-        }
+  /**
+   * @param {Vector2} newScale
+   */
+  set scale (newScale) {
+    if (newScale instanceof Vector2) {
+      this._scale = newScale
+    } else {
+      throw new TypeError()
     }
+  }
 
-    /**
-     * @param {number} delta 
-     */
-    rotate(delta) {
-        if (typeof delta == 'number') {
-            this.rotation += delta
-        } else {
-            throw new TypeError()
-        }
+  /**
+   * @param {Vector2} delta
+   */
+  translate (delta) {
+    if (delta instanceof Vector2) {
+      this.position.x += delta.x
+      this.position.y += delta.y
+    } else {
+      throw new TypeError()
     }
+  }
 
-    /**
-     * @param {number} scalar
-     */
-    scalarScale(scalar) {
-        if (typeof scalar == 'number') {
-            this.scale.x *= scalar
-            this.scale.y *= scalar
-        } else {
-            throw new TypeError()
-        }
+  /**
+   * @param {number} delta
+   */
+  rotate (delta) {
+    if (typeof delta === 'number') {
+      this.rotation += delta
+    } else {
+      throw new TypeError()
     }
+  }
 
-    /**
-     * @param {Vector2} vector
-     */
-    vectorScale(vector) {
-        if (vector instanceof Vector2) {
-            this.scale.x *= vector.x
-            this.scale.y *= vector.y
-        } else {
-            throw new TypeError()
-        }
+  /**
+   * @param {number} scalar
+   */
+  scalarScale (scalar) {
+    if (typeof scalar === 'number') {
+      this.scale.x *= scalar
+      this.scale.y *= scalar
+    } else {
+      throw new TypeError()
     }
+  }
+
+  /**
+   * @param {Vector2} vector
+   */
+  vectorScale (vector) {
+    if (vector instanceof Vector2) {
+      this.scale.x *= vector.x
+      this.scale.y *= vector.y
+    } else {
+      throw new TypeError()
+    }
+  }
 }
