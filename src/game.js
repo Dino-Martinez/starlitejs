d6515352204ae964a3ddb6043837a590a2ab53b0
Dino-Martinez <>
2021-03-25 00:50:39 -0700

    [Added] Scene class and simple game controller

---
+++ src/game.js
@@ -0,0 +1,35 @@
+import Scene from './scene.js'
+import Layer from './layer.js'
+import Entity from './entity.js'
+import Vector2 from './vector2.js'
+
+const s = new Scene()
+const l1 = new Layer('Background', 0)
+const l2 = new Layer('Foreground', 1)
+
+const b = new Entity()
+b.size = new Vector2(600, 800)
+b.color = '#dddddd'
+
+const e = new Entity()
+e.x = 50
+e.y = 150
+e.size = new Vector2(50, 100)
+e.rotation = 50
+e.color = '#ffbad2'
+
+const e2 = new Entity()
+e2.x = 150
+e2.y = 150
+e2.size = new Vector2(50, 150)
+e2.rotation = 90
+e2.color = '#00214d'
+
+l2.addEntity(e2)
+l2.addEntity(e)
+l1.addEntity(b)
+
+s.addLayer(l2)
+s.addLayer(l1)
+
+s.render()
--- src/scene.js
+++ src/scene.js
@@ -1,34 +1,18 @@
 import Layer from './layer.js'
 import Entity from './entity.js'
 
-const div = document.getElementById('container')
-const canvas = document.createElement('canvas')
-canvas.id = 'CursorLayer'
-canvas.width = 600
-canvas.height = 800
-canvas.style.zIndex = 8
-canvas.style.position = 'absolute'
-canvas.style.border = '1px solid'
-const ctx = canvas.getContext('2d')
-ctx.fillStyle = '#baf5ff'
-ctx.fillRect(0, 0, canvas.width, canvas.height)
-div.appendChild(canvas)
+export default class Scene {
+  constructor () {
+    this.layers = []
+  }
 
-const e = new Entity()
-e.x = 50
-e.y = 150
-e.size = new Vector2(50, 100)
-e.rotation = 50
+  addLayer (layer) {
+    this.layers.push(layer)
+    this.layers.sort((a, b) => (a.priority < b.priority ? -1 : 1))
+  }
 
-const e2 = new Entity()
-e2.x = 150
-e2.y = 150
-e2.size = new Vector2(50, 150)
-e2.rotation = 180
-
-const l1 = new Layer('Middle Ground', ctx)
-
-l1.addEntity(e)
-l1.addEntity(e2)
-
-l1.render()
+  render () {
+    console.log('Rendering')
+    this.layers.forEach(layer => layer.render())
+  }
+}
